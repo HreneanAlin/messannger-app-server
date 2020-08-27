91037f280741c548104b44e4c5073a6a543243e0
HreneanAlin <hreneanalin@gmail.com>
2020-08-27 19:37:38 +0300

    dfsfnjdfnjgnjbg

--- router.js
+++ router.js
@@ -3,10 +3,10 @@ const express = require('express')
 const router = express.Router()
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
-//const urlencodedParser = bodyParser.urlencoded({extended: false})
+const urlencodedParser = bodyParser.urlencoded({extended: false})
 const {createUserDb, getUserDb} = require('./repository/usersRepository')
 
-
+console.log('dfijsdiujfujdfj')
 
 router.get('/', (req, res) => {
 
